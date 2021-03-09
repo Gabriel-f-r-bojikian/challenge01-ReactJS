e542e36afd27a79c3c616aef557588348fa14087
Gabriel Fernandes Rosa Bojikian <gabriel.f.r.bojikian@gmail.com>
2021-03-09 19:39:43 -0300

    Completed the challenge and added UUID functionalities
    The functions handleToggleTaskCompletion(), handleRemoveTask() and
    handleToggleTaskCompletion() have been implemented. All three of
    them do their functions by creating a list of tasks out of the page
    state, modifying the list in the intended way and then setting that
    list to be the new program state.
    
    Furthermore, a UUID package was added to the application in order
    to make each task id unique.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-import '../styles/tasklist.scss'
+import '../styles/tasklist.scss';
 
-import { FiTrash, FiCheckSquare } from 'react-icons/fi'
+import { FiTrash, FiCheckSquare } from 'react-icons/fi';
+
+import {v4 as uuid} from 'uuid';
 
 interface Task {
   id: number;
@@ -14,16 +16,58 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  function createPlaceHolderTaskList(){
+    let placeholderTaskListForThisFunction: Task[] = [...tasks];
+    return placeholderTaskListForThisFunction;
+  }
+
+  function createNewTaskObject() {
+    const newTaskBeingCreated: Task = {
+      id: uuid(),
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    return newTaskBeingCreated;
+  }
+
+  function addNewTaskToTaskList(item: Task){
+    let placeholderList: Task[] = createPlaceHolderTaskList();
+
+    placeholderList.push(item); 
+
+    setTasks(placeholderList);
+  }
+  
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle !== ''){ 
+      let newTask: Task = createNewTaskObject();
+
+      addNewTaskToTaskList(newTask);
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    let placeholderList: Task[] = createPlaceHolderTaskList();
+
+    const indexOfTaskToBeCompleted: number = placeholderList.map((item) => item.id).indexOf(id);
+
+    placeholderList[indexOfTaskToBeCompleted].isComplete = true;
+
+    setTasks(placeholderList);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    let placeholderList: Task[] = createPlaceHolderTaskList();
+
+    const indexOfTaskToBeRemoved: number = placeholderList.map((item) => item.id).indexOf(id);
+    
+    placeholderList.splice(indexOfTaskToBeRemoved, 1);
+
+    setTasks(placeholderList);
   }
 
   return (
